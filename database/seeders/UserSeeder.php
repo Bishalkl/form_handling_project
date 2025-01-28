76eb1cb962a6c51e365046e5c824de5c02a7c5b3
Bishalkl <Bishal123koirala@gmail.com>
2025-01-28 13:27:53 +0545

    Done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\user;
+use Illuminate\Support\Facades\File;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = File::get(path:'database/json/users.json');
+        $users  = collect(json_decode($json));
+
+        // using collection
+        $users->each(function($user) {
+            user::create([
+                'name' => $user->name,
+                'email' =>$user->email,
+                'city' => $user->city,
+                'age' => $user->age,
+            ]);
+        });
+    }
+}
